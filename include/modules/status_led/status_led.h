d302a9f1556c535e9fb3cdb084f717846f8b361b
Vaseksch <venda.sicha@gmail.com>
2026-03-02 00:16:14 +0100

    feat: add status led pulse func

--- include/modules/status_led/status_led.h
+++ include/modules/status_led/status_led.h
@@ -2,5 +2,6 @@
 #define STATUS_LED_H
 
 void status_led_blink(int blinks_total);
+void status_led_pulse(int blinks_total);
 
 #endif
--- src/modules/status_led/status_led.cpp
+++ src/modules/status_led/status_led.cpp
@@ -10,3 +10,16 @@ void status_led_blink(int blinks_total){
         delay(150);
     }
 }
+
+void status_led_pulse(int blinks_total){
+    for(int blink = 0; blink < blinks_total; blink++){
+        for(int duty = 0; duty < 255; duty++){
+            analogWrite(status_led, duty);
+            delay(25);
+        }
+        for(int duty = 0; duty < 255; duty++){
+            analogWrite(status_led, 225 - duty);
+            delay(25);
+        }
+    }
+}
