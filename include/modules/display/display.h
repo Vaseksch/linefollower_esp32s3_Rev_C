07eabde7774a43f67c7d7c84cdedc578cea17df5
Vaseksch <venda.sicha@gmail.com>
2026-03-24 10:57:03 +0100

    feat: add display persence check

--- include/modules/display/display.h
+++ include/modules/display/display.h
@@ -1,6 +1,10 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#define DISPLAY_ADDRESS 0x3c
+
+extern bool display_avalible;
+
 void oled_begin();
 void wait_screen(void);
 void calibration_complete_screen(void);
